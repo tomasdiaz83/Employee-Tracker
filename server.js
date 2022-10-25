03d1f9b001ed054d25491f3246513f371518fb1a
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-10-25 11:23:33 -0500

    Setting up server.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
+      },
+      console.log(`Connected to the employee database.`)
+);
+
